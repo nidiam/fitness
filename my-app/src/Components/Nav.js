ac611f19f042b1063bdb3faf266649ee096d57c3
Nidia Menchaca <nmenchaca@Nidias-Air.lan>
2024-03-24 18:13:44 -0500

    added logo and widened nav bar

--- my-app/src/Components/Nav.js
+++ my-app/src/Components/Nav.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import Box from '@mui/material/Box'
+import React from 'react';
 import Button from '@mui/material/Button';
-import './Nav.css'
+import { AppBar } from '@mui/material';
+import './Nav.css';
+const logo = require("./logo.jpg")
 
 const Nav = () => {
   return (
-    <div> <Box className="Nav">
-      
-      <Button className="Button" style={{ backgroundColor: 'pink', color: 'white', marginLeft:'10%' }}> Home </Button>
-      <Button className="Button" style={{ backgroundColor: 'pink', color: 'white', marginLeft:'10%' }}> Classes </Button>
-      <Button className="Button" style={{ backgroundColor: 'pink', color: 'white', marginLeft:'10%' }}> Locations </Button>
-      <Button className="Button" style={{ backgroundColor: 'pink', color: 'white', marginLeft:'10%' }}> Instructors </Button>
-      <Button className="Button" style={{ backgroundColor: 'pink', color: 'white', marginLeft:'10%' }}> Contact </Button>
-     
-      
-      
-    
-    </Box> </div>
-  )
-}
+    <AppBar className="Nav" style={{ height: '13vh', marginBottom:'100px',backgroundColor:'white' }}>
+      <div className="nav-buttons">
+        <img src={logo} alt='logo' style={{ height: '80px', width: '80px', position:'relative', right: '300px' }}></img>
+        <Button style={{ backgroundColor: 'pink', color:'white', marginLeft: '20px' }} className="Button">Home</Button>
+        <Button style={{ backgroundColor: 'pink', color:'white', marginLeft: '20px' }} className="Button">Classes</Button>
+        <Button style={{ backgroundColor: 'pink', color:'white', marginLeft: '20px' }} className="Button">Locations</Button>
+        <Button style={{ backgroundColor: 'pink', color:'white', marginLeft: '20px' }} className="Button">Our Instructors</Button>
+        <Button style={{ backgroundColor: 'pink', color:'white', marginLeft: '20px' }} className="Button">Contact Us</Button>
+        
+      </div>
+    </AppBar>
+  );
+};
 
-export default Nav
+export default Nav;
